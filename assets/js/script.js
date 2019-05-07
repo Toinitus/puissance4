0fadbd8dde0b119f78e836a9eb99753914e92599
Toinitus <antoinego96@orange.fr>
2019-05-07 12:27:10 +0200

    js2

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,20 @@ var jeu = true;
 var text = "";
 var plateau = [];
 
-for (var i = 0; i < nbLigne.length; i++) {
-	plateau[i] += [];
+for (var i = 0; i < nbLigne; i++) {
+	plateau[i] = [];
+}
+
+newGame();
+
+function newGame(){
+	for (var i = 0; i < nbLigne; i++) {
+		for (var j = 0; j < nbColonne; j++) {
+			plateau[i][j] = 0;
+			
+		}
+	}
+	numJoueur = 1;
+	afficheTextAnnonce("Le jeu commence ! c'est au tour du joueur"+nomJoueur(numJoueur));
+	jeu = true;
 }
