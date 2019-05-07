f384442b98f08009664c5e120822a615a63974a9
Toinitus <antoinego96@orange.fr>
2019-05-07 16:03:49 +0200

    jsdurdur

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,48 @@ function creerTableau(){
 	text += "</table>";
 	document.getElementById("puissance4").innerHTML = text;
 }
+
+function detectClik(j){
+	if (verifPosition(j) && jeu) {
+		var ligneEnCours = poseJeton(j); //numero de la ligne en cours
+		var verifEnd = puissance4(ligneEnCours, j, 0, 0);
+		if (verifEnd) {
+			jeu = false;
+			afficheTextAnnonce("Le joueur "+nomJoueur(numJoueur)+" a gagné la partie !");
+		}else{
+			if (numJoueur == 1) {
+				numJoueur = 2;
+			}else{
+				numJoueur = 1;
+			}
+		afficheTextAnnonce("C'est au tour du joueur "+nomJoueur(numJoueur));	
+		}
+	}
+}
+
+function verifPosition(j){ //si case en haut de la colonne est vide on retourne vrais sinon faux
+	if (plateau[0][j] == 0) {
+		return true;
+	}else{
+		return false;
+	} 
+}
+
+function poseJeton(j){
+	for (var i = nbLigne-1; i >= 0; i--) {
+		if (plateau[i][j] == 0) {
+			plateau[i][j] = numJoueur;
+			refreshTableau(i, j, numJoueur);
+			return i;
+		}
+	}
+}
+
+function refreshTableau(x, y, i){
+	document.getElementById(x+'-'+y).innerHTML = "<div class='joueur'>"+i+"</div>"
+}
+
+function puissance4(ligne, colonne, l, c){
+	console.log('valeur: '+ligne+''+colonne+' / incremente '+l+' '+c);
+	return false;
+}
